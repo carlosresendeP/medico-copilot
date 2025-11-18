365c9a818981ab2eac189103eceecb27a0be6082
carlosresendeP <carlosresende.paula@gmail.com>
2025-11-18 20:34:59 -0300

    api para o deploy

--- interface/src/services/api.ts
+++ interface/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333", // Backend Fastify
+  baseURL: "https://medico-copilot.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
